8e49a0cdf21608316939d120665ecb243c408182
Tássio Gonçalves <tassiogoncalvesg@gmail.com>
2017-09-18 18:05:23 -0300

    Ex7 Class Components

--- ex/family.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { childrenWithProps } from '../utils/reactUtils'
-
-export default props => (
-    <div>
-      <h3>Família</h3>
-      { childrenWithProps(props) }
-    </div>
-  )
